feb4f68746f71108114d639f1e4dca60eadd3926
ali-rezaei-dev <ali.rezaei.developer@gmail.com>
2021-10-19 20:16:44 +0330

    Hangman game

---
+++ README.md
@@ -0,0 +1,3 @@
+# Hangman game in pure JavaScript
+
+A simple but clever Hangman game made with pure JavaScript
---
+++ hangman.js
@@ -0,0 +1,326 @@
+const logo = `
+ _                                             
+| |                                            
+| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
+| '_ \\ / _' | '_ \\ / _' | '_ ' _ \\ / _' | '_ \\ 
+| | | | (_| | | | | (_| | | | | | | (_| | | | |
+|_| |_|\\__,_|_| |_|\\__, |_| |_| |_|\\__,_|_| |_|
+                    __/ |                      
+                   |___/  
+`;
+console.log(logo);
+
+const stage = [
+  ` 
+      +---+
+      |   |
+          |
+          |
+          |
+          |
+    =========`,
+  `
+      +---+
+      |   |
+      O   |
+          |
+          |
+          |
+    =========`,
+  `
+      +---+
+      |   |
+      O   |
+      |   |
+          |
+          |
+    =========`,
+  `
+      +---+
+      |   |
+      O   |
+      |\\  |
+          |
+          |
+    =========`,
+  `
+      +---+
+      |   |
+      O   |
+     /|\\  |
+          |
+          |
+    =========`,
+  `
+      +---+
+      |   |
+      O   |
+     /|\\  |
+       \\  |
+          |
+    =========`,
+  `
+      +---+
+      |   |
+      O   |
+     /|\\  |
+     / \\  |
+          |
+    =========`,
+];
+
+const wordList = [
+  "abruptly",
+  "absurd",
+  "abyss",
+  "affix",
+  "askew",
+  "avenue",
+  "awkward",
+  "axiom",
+  "azure",
+  "bagpipes",
+  "bandwagon",
+  "banjo",
+  "bayou",
+  "beekeeper",
+  "blitz",
+  "blizzard",
+  "boggle",
+  "bookworm",
+  "boxcar",
+  "boxful",
+  "buckaroo",
+  "buffalo",
+  "buffoon",
+  "buxom",
+  "buzzard",
+  "buzzing",
+  "buzzwords",
+  "caliph",
+  "cobweb",
+  "cockiness",
+  "croquet",
+  "crypt",
+  "curacao",
+  "cycle",
+  "daiquiri",
+  "dirndl",
+  "disavow",
+  "dizzying",
+  "duplex",
+  "dwarves",
+  "embezzle",
+  "equip",
+  "espionage",
+  "euouae",
+  "exodus",
+  "faking",
+  "fishhook",
+  "fixable",
+  "fjord",
+  "flapjack",
+  "flopping",
+  "fluffiness",
+  "flyby",
+  "foxglove",
+  "frazzled",
+  "frizzled",
+  "fuchsia",
+  "funny",
+  "gabby",
+  "galaxy",
+  "galvanize",
+  "gazebo",
+  "giaour",
+  "gizmo",
+  "glowworm",
+  "glyph",
+  "gnarly",
+  "gnostic",
+  "gossip",
+  "grogginess",
+  "haiku",
+  "haphazard",
+  "hyphen",
+  "iatrogenic",
+  "icebox",
+  "injury",
+  "ivory",
+  "ivy",
+  "jackpot",
+  "jaundice",
+  "jawbreaker",
+  "jaywalk",
+  "jazziest",
+  "jazzy",
+  "jelly",
+  "jigsaw",
+  "jinx",
+  "jiujitsu",
+  "jockey",
+  "jogging",
+  "joking",
+  "jovial",
+  "joyful",
+  "juicy",
+  "jukebox",
+  "jumbo",
+  "kayak",
+  "kazoo",
+  "keyhole",
+  "khaki",
+  "kilobyte",
+  "kiosk",
+  "kitsch",
+  "kiwifruit",
+  "klutz",
+  "knapsack",
+  "larynx",
+  "lengths",
+  "lucky",
+  "luxury",
+  "lymph",
+  "marquis",
+  "matrix",
+  "megahertz",
+  "microwave",
+  "mnemonic",
+  "mystify",
+  "naphtha",
+  "nightclub",
+  "nowadays",
+  "numbskull",
+  "nymph",
+  "onyx",
+  "ovary",
+  "oxidize",
+  "oxygen",
+  "pajama",
+  "peekaboo",
+  "phlegm",
+  "pixel",
+  "pizazz",
+  "pneumonia",
+  "polka",
+  "pshaw",
+  "psyche",
+  "puppy",
+  "puzzling",
+  "quartz",
+  "queue",
+  "quips",
+  "quixotic",
+  "quiz",
+  "quizzes",
+  "quorum",
+  "razzmatazz",
+  "rhubarb",
+  "rhythm",
+  "rickshaw",
+  "schnapps",
+  "scratch",
+  "shiv",
+  "snazzy",
+  "sphinx",
+  "spritz",
+  "wimpy",
+  "witchcraft",
+  "wizard",
+  "stretch",
+  "stronghold",
+  "stymied",
+  "subway",
+  "swivel",
+  "syndrome",
+  "thriftless",
+  "thumbscrew",
+  "topaz",
+  "transcript",
+  "transgress",
+  "transplant",
+  "triphthong",
+  "twelfth",
+  "twelfths",
+  "unknown",
+  "unworthy",
+  "wimpy",
+  "witchcraft",
+  "wizard",
+  "unzip",
+  "uptown",
+  "vaporize",
+  "vixen",
+  "vodka",
+  "voodoo",
+  "vortex",
+  "voyeurism",
+  "walkway",
+  "waltz",
+  "wave",
+  "wavy",
+  "waxy",
+  "wellspring",
+  "wheezy",
+  "whiskey",
+  "whizzing",
+  "whomever",
+  "wimpy",
+  "witchcraft",
+  "wizard",
+  "woozy",
+  "wristwatch",
+  "wyvern",
+  "xylophone",
+  "yachtsman",
+  "yippee",
+  "yoked",
+  "youthful",
+  "yummy",
+  "zephyr",
+  "zigzag",
+  "zigzagging",
+  "zilch",
+  "zipper",
+  "zodiac",
+  "zombie",
+];
+
+const chosenWord = wordList[parseInt(Math.random() * wordList.length)];
+let wrongGuessCount = 0;
+const opportunities = [];
+const userWords = [];
+console.log(chosenWord);
+
+for (let i = 0; i < chosenWord.length; i++) {
+  opportunities.push("_");
+}
+
+while (opportunities.join("") !== chosenWord && wrongGuessCount !== 6) {
+  console.log(opportunities.join(", "));
+
+  const userGuess = prompt("Enter your guess!");
+
+  if (chosenWord.includes(userGuess)) {
+    const locations = [];
+
+    for (let i = 0; i < chosenWord.length; i++) {
+      if (chosenWord[i] === userGuess) {
+        locations.push(i);
+      }
+    }
+
+    for (let i = 0; i < locations.length; i++) {
+      opportunities[locations[i]] = userGuess;
+    }
+
+    console.log(stage[wrongGuessCount]);
+  } else {
+    wrongGuessCount++;
+    console.log(stage[wrongGuessCount]);
+  }
+}
+
+console.log(
+  wrongGuessCount === 6 ? "You lost!" : `You won, the word was: ${chosenWord} `
+);
